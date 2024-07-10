c58e79b39ffb72efa75c03bf8ac6bffc3d5abde2
Minju Kim <mnat713@naver.com>
2024-07-10 15:10:42 +0900

    Add requirements.txt and static

--- postboxProject/postboxProject/urls.py
+++ postboxProject/postboxProject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin777mj/', admin.site.urls),
     path('', include('posts.urls')),
 ]
